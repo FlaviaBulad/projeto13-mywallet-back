fdd28d072bd933d62ed385c43d1afde9532f0d51
Flavia Bulad <flavia.bulad@gmail.com>
2022-06-28 21:43:35 -0300

    feat: post /sign-in route

--- API/app.js
+++ API/app.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { json } from "express";
 import chalk from "chalk";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
@@ -13,10 +13,34 @@ app.use(express.json());
 
 const PORT = process.env.PORT;
 
-const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
 mongoClient.connect(() => {
   db = mongoClient.db("my-wallet-api");
+  console.log(chalk.magenta.bold("MongoDB connected"));
+});
+
+app.post("/sign-up", async (req, res) => {
+  const user = req.body;
+
+  const passwordHash = bcrypt.hashSync(user.password, 10);
+
+  await db.collection("users").insertOne({ ...user, password: passwordHash });
+
+  res.sendStatus(201);
+});
+
+app.post("/sign-in", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await db.collection("users").findOne({ email });
+
+  if (user && bcrypt.compareSync(password, user.password)) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(401);
+  }
 });
 
 app.listen(process.env.PORT, () => {
