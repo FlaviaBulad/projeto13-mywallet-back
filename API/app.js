714640ddbc8454c2f6c234112757134549edbf69
Flavia Bulad <flavia.bulad@gmail.com>
2022-07-02 14:20:26 -0300

    feat: post /sign-up validation

--- API/app.js
+++ API/app.js
@@ -4,6 +4,7 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 import cors from "cors";
+import joi from "joi";
 
 dotenv.config();
 
@@ -12,9 +13,10 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
 
 let db;
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+const mongoClient = new MongoClient(MONGO_URI);
 
 mongoClient.connect(() => {
   db = mongoClient.db("my-wallet-api");
@@ -24,11 +26,36 @@ mongoClient.connect(() => {
 app.post("/sign-up", async (req, res) => {
   const user = req.body;
 
-  const passwordHash = bcrypt.hashSync(user.password, 10);
+  const signupSchema = joi.object({
+    name: joi.string().min(1).required(),
+    email: joi.string().email().required(),
+    password: joi.string().min(3).max(15).required().label("Password"),
+    password_confirmation: joi
+      .any()
+      .equal(joi.ref("password"))
+      .required()
+      .label("Confirm password")
+      .options({ messages: { "any.only": "{{#label}} does not match" } }),
+  });
 
-  await db.collection("users").insertOne({ ...user, password: passwordHash });
+  const validation = signupSchema.validate(user, {
+    abortEarly: true,
+  });
 
-  res.sendStatus(201);
+  if (validation.error) {
+    console.log(validation.error.details);
+    return res.sendStatus(422);
+  }
+
+  try {
+    const passwordHash = bcrypt.hashSync(user.password, 10);
+
+    await db.collection("users").insertOne({ ...user, password: passwordHash });
+
+    res.sendStatus(201);
+  } catch (error) {
+    res.status(500).send("Server error", error);
+  }
 });
 
 app.post("/sign-in", async (req, res) => {
