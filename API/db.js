78d8abf31b3e4211d23a20b6d4e965409013066b
Flavia Bulad <flavia.bulad@gmail.com>
2022-07-05 21:27:25 -0300

    initial commit

--- API/db.js
+++ API/db.js
@@ -7,7 +7,9 @@ dotenv.config();
 const MONGO_URI = process.env.MONGO_URI;
 
 let db;
-const mongoClient = new MongoClient(MONGO_URI);
+const mongoClient = new MongoClient(
+  "mongodb+srv://admin:admin@cluster0.top9izf.mongodb.net/?retryWrites=true&w=majority"
+);
 
 mongoClient.connect(() => {
   db = mongoClient.db("my-wallet-api");
